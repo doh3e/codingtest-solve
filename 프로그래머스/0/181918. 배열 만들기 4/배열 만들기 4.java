73ec56f4e93296801346d64046dcc7002429c10c
Jieun Jung <wldms3333@gmail.com>
2025-11-12 15:37:53 +0900

    [level 0] Title: 배열 만들기 4, Time: 37.57 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         
-        ArrayList<Integer> stk = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
         int i = 0;
         while(i < arr.length){
-            if (stk.isEmpty()) {
-                stk.add(arr[i]);
-                i++;
-            } else if (stk.get(stk.size() - 1) < arr[i]) {
-                stk.add(arr[i]);
+            if (stk.isEmpty() || stk.peek() < arr[i]) {
+                stk.push(arr[i]);
                 i++;
             } else {
-                stk.remove(stk.size() - 1);
+                stk.pop();
             }
         }
         
-        return stk.stream().mapToInt(Integer::intValue).toArray();
+        return stk.stream().mapToInt(j -> j).toArray();
     }
 }
